9871d939bd215ac0111867cb8c50cbdd170bec26
John Kearon <jkearon@hotmail.com>
2025-04-11 11:44:05 -0400

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by John Kearon on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
