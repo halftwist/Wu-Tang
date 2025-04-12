a5234ef25196a391edbd8e3f4e625e019be1350a
John Kearon <jkearon@hotmail.com>
2025-04-12 11:15:23 -0400

    Swift lesson 53 last question

--- Wu-Tang/ContentView.swift
+++ Wu-Tang/ContentView.swift
@@ -9,91 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var enteredName: String = ""
-    @State private var staticCoderNameIs = "Your Wu-Tang Coder Name is:"
+    @State private var enteredName = ""
+    @State private var staticCoderNameIs = ""
     @State private var wuTangName = "Undefined Breakpoint"
+    @FocusState private var isFocused: Bool
     private let firstColumn = ["Algorithmic",
-                                      "Byte",
-                                      "Cache",
-                                      "Debug",
-                                      "Echo",
-                                      "Function",
-                                      "Git",
-                                      "Hex",
-                                      "Infinite",
-                                      "Java",
-                                      "Kernel",
-                                      "Logic",
-                                      "Module",
-                                      "Node",
-                                      "Object",
-                                      "Pixel",
-                                      "Query",
-                                      "Runtime",
-                                      "Script",
-                                      "Token",
-                                      "Undefined",
-                                      "Virtual",
-                                      "Web",
-                                      "Xcode",
-                                      "Yota",
-                                      "Zero"]
+                               "Byte",
+                               "Cache",
+                               "Debug",
+                               "Echo",
+                               "Function",
+                               "Git",
+                               "Hex",
+                               "Infinite",
+                               "Java",
+                               "Kernel",
+                               "Logic",
+                               "Module",
+                               "Node",
+                               "Object",
+                               "Pixel",
+                               "Query",
+                               "Runtime",
+                               "Script",
+                               "Token",
+                               "Undefined",
+                               "Virtual",
+                               "Web",
+                               "Xcode",
+                               "Yota",
+                               "Zero"]
     private let secondColumn = ["$tack",
-                                       "Processor",
-                                       "Cipher",
-                                       "Daemon",
-                                       "EndPoint",
-                                       "Framework",
-                                       "Gateway",
-                                       "Hub",
-                                       "Interrupt",
-                                       "Crash",
-                                       "Loop",
-                                       "Module",
-                                       "Nexus",
-                                       "Optimizer",
-                                       "Protocol",
-                                       "Queue",
-                                       "Router",
-                                       "Stack",
-                                       "Thread",
-                                       "Update",
-                                       "Variable",
-                                       "Widget",
-                                       "Terminal",
-                                       "Yield",
-                                       "Zen",
-                                       "Ace",
-                                       "Breakpoint",
-                                       "Root",
-                                       "Instance",
-                                       "Access",
-                                       "Archive",
-                                       "Control",
-                                       "Justice"]
+                                "Processor",
+                                "Cipher",
+                                "Daemon",
+                                "EndPoint",
+                                "Framework",
+                                "Gateway",
+                                "Hub",
+                                "Interrupt",
+                                "Crash",
+                                "Loop",
+                                "Module",
+                                "Nexus",
+                                "Optimizer",
+                                "Protocol",
+                                "Queue",
+                                "Router",
+                                "Stack",
+                                "Thread",
+                                "Update",
+                                "Variable",
+                                "Widget",
+                                "Terminal",
+                                "Yield",
+                                "Zen",
+                                "Ace",
+                                "Breakpoint",
+                                "Root",
+                                "Instance",
+                                "Access",
+                                "Archive",
+                                "Control",
+                                "Justice"]
     
     var body: some View {
         VStack {
             Text("Wu-Tang\nCoder Name Generator")
-            .font(.title)
-            .foregroundStyle(.yellow)
-            .fontWeight(.black)
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity)
-            .padding(.bottom)
-            .padding(.horizontal)
-            .background(.black)
-                 
+                .font(.title)
+                .foregroundStyle(.yellow)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .padding(.horizontal)
+                .background(.black)
+            
             Spacer()
             
-           TextField("Enter name here", text: $enteredName)
+            TextField("Enter name here", text: $enteredName)
                 .font(.title2)
                 .textFieldStyle(.roundedBorder)
                 .overlay {
                     RoundedRectangle(cornerRadius: 5)
                         .stroke(.gray, lineWidth: 1)
                 }
+//                .focused(FocusState<Bool>.Binding)
                 .padding()
+                .focused($isFocused)
             
             Spacer()
             
@@ -102,43 +105,93 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .padding()
                 .frame(maxWidth: .infinity, alignment: .init(horizontal: .center, vertical: .center))
-
+            
             Spacer()
             
             Button {
-               //TODO: <#code#>
+                //TODO: <#code#>
+//                wuTangName = getWuTangName(enteredName: enteredName)
+                wuTangName = getWuTangName2(name: enteredName)
+                isFocused = false
+                staticCoderNameIs = "Your Wu-Tang Coder Name is:"
             } label: {
                 Image("wu-tang-button")
-                 Text("Get It!")
-              }
+                Text("Get It!")
+            }
             .font(.title2)
             .fontWeight(.bold)
             .foregroundStyle(.yellow)
             .buttonStyle(.borderedProminent)
             .tint(.black)
-            .background(Color.black)
+            .disabled(enteredName.isEmpty)
             
             Spacer()
             
             VStack {
                 Text(staticCoderNameIs)
                 Text(wuTangName)
-                     .fontWeight(.black)
+                    .fontWeight(.black)
             }
             .frame(height: 130)
             .font(.largeTitle)
-                        
+            
             Spacer()
-                        
+            
             Image("wu-tang")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
             
-             Spacer()
-         }
-//        .background(Color.black)
-//        .padding()
+            Spacer()
+        }
+        //        .background(Color.black)
+        //        .padding()
     }
+    
+    func getWuTangName2(name: String) -> String {
+        var localName = name
+        let firstLetter = localName.removeFirst()
+        print ("first letter is \(firstLetter)")
+        var firstColumnIndex = 0
+        // loop through all elements of firstColumn & compare first letter
+        for i in 0..<firstColumn.count {
+            if firstColumn[i].first == firstLetter {
+                firstColumnIndex = i == 0 ? 25 : i - 1
+            }
+        }
+        return "\(firstColumn[firstColumnIndex]) \(secondColumn.randomElement()!)"
+    }
+    
+    
+    func getWuTangName(enteredName: String) -> String {   // Johns Version
+        var firstLetterOfName = self.enteredName.removeFirst()
+        var indexOfPreviousLetter = 0
+        var wuTangName: String = ""
+        
+        for letter in firstColumn {
+            
+            if firstLetterOfName == letter.first
+            {
+                //                print("match on \(indexOfPreviousLetter) \(firstLetterOfName) and \(letter) ")
+                if indexOfPreviousLetter == 0 {
+                    indexOfPreviousLetter = firstColumn.count - 1
+                    wuTangName = String(firstColumn[indexOfPreviousLetter])
+                } else {
+                    indexOfPreviousLetter -= 1
+                    wuTangName = String(firstColumn[indexOfPreviousLetter])
+                }
+            }
+            //            print("\(String(describing: firstLetterOfName)) \(letter)")
+            indexOfPreviousLetter += 1
+            
+        }
+        
+        var secondIndex = Int.random(in: 0..<secondColumn.count - 1)
+        var secondName = secondColumn[secondIndex]
+        
+        return String(wuTangName + " " + secondName)
+        
+    }
+    
 }
 
 #Preview {
